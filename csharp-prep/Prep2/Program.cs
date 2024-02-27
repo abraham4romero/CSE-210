2fef04a4ec50685d00163aef145a60bcb57d57e0
Abraham Romero <abraham4romero@gmail.com>
2024-02-27 11:17:01 -0700

    I gave a letter grade according to a percentage.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your current grade as a percentage?");
+        string ARinput = Console.ReadLine();
+        int ARpercentage = int.Parse(ARinput);
+        string ARgrade;
+        if (ARpercentage >= 90) {
+            ARgrade = "A";
+        } else if (ARpercentage >= 80) {
+            ARgrade = "B";
+        } else if (ARpercentage >= 70) {
+            ARgrade = "C";
+        } else if (ARpercentage >= 60) {
+            ARgrade = "D";
+        } else {
+            ARgrade = "F";
+        }
+
+        Console.WriteLine($"With {ARinput}, your grade is {ARgrade}.");
     }
 }
