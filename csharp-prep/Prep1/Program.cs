f860b6fad83152a72b447fdc67717bcb046ddd15
Abraham Romero <abraham4romero@gmail.com>
2024-02-27 11:06:25 -0700

    I completed the project with inputs for first and last names and outputting full name.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name?");
+        string ARfirst = Console.ReadLine();
+        Console.WriteLine("What is your last name?");
+        string ARlast = Console.ReadLine();
+        Console.WriteLine("");
+        Console.WriteLine($"Your name is {ARlast}, {ARfirst} {ARlast}");
     }
 }
