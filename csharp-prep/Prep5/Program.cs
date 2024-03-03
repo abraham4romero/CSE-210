10b42cebf1740e81f52434f90dff9470b78807fa
Abraham Romero <abraham4romero@gmail.com>
2024-03-03 02:21:01 -0700

    I completed the prep assignment.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep5 World!");
+    {  
+        DisplayWelcome();
+        string name = GetName();
+        int num = GetNum();
+
+        int numsqr = SquareNum(num);
+
+        DisplayResults(name, numsqr);
+
+    }
+
+    static void DisplayWelcome() {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string GetName() {
+        Console.WriteLine("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int GetNum() {
+        Console.WriteLine("Please enter your favorite number: ");
+        int num = int.Parse(Console.ReadLine());
+
+        return num;
+    }
+
+    static int SquareNum(int num) {
+        int numsqr = num * num;
+
+        return numsqr;
+    }
+    static void DisplayResults(string name, int numsqr) {
+        Console.WriteLine($"{name}, the square of your number is {numsqr}");
     }
 }
