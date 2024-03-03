1eb02ad7562622e8026062ea84d25024b3d0d064
Abraham Romero <abraham4romero@gmail.com>
2024-03-03 01:47:06 -0700

    I completed the prep assignment.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        bool list0 = false;
+        int sum = 0;
+        int avg = 0;
+        int max = 0;
+        List<int> numbers =  new List<int>();
+
+        while(!list0) {
+            Console.WriteLine("Enter number: ");
+            int entry = int.Parse(Console.ReadLine());
+            if (entry == 0){
+                list0 = true;
+            } else {
+                numbers.Add(entry);
+            }
+        }
+
+        foreach (int num in numbers) {
+            if (num > max) {
+                max = num;
+            }
+            sum += num;
+        }
+
+        avg = sum / numbers.Count;
+
+        Console.WriteLine($"Sum: {sum}");
+        Console.WriteLine($"Average: {avg}");
+        Console.WriteLine($"Largest number: {max}");
+
     }
 }
